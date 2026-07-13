55a92117054b30c86fc3c36e681c61236c3cc570
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2026-07-14 00:46:21 +1200

    feat: add sorting to table columns

--- apps/Frontend/src/features/friends/components/columns.tsx
+++ apps/Frontend/src/features/friends/components/columns.tsx
@@ -1,13 +1,35 @@
 import type { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "#/components/ui/button";
 import type { Friend } from "../types";
 
 export const columns: ColumnDef<Friend>[] = [
 	{
 		accessorKey: "id",
-		header: "ID",
+		header: ({ column }) => {
+			return (
+				<Button
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					variant="ghost"
+				>
+					ID
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
 	},
 	{
 		accessorKey: "name",
-		header: "Name",
+		header: ({ column }) => {
+			return (
+				<Button
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					variant="ghost"
+				>
+					Name
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
 	},
 ];
--- apps/Frontend/src/features/friends/components/data-table.tsx
+++ apps/Frontend/src/features/friends/components/data-table.tsx
@@ -4,9 +4,11 @@ import {
 	type ColumnDef,
 	flexRender,
 	getCoreRowModel,
+	getSortedRowModel,
+	type SortingState,
 	useReactTable,
 } from "@tanstack/react-table";
-
+import { useState } from "react";
 import {
 	Table,
 	TableBody,
@@ -25,10 +27,17 @@ export function DataTable<TData, TValue>({
 	columns,
 	data,
 }: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([]);
+
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		state: {
+			sorting,
+		},
 	});
 
 	return (
